1126fb478642cad3c042c66d721293fad9a4faf9
remotalogin <remotamobile@gmail.com>
2025-06-18 00:15:10 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
-require("dotenv").config();
-
+// config.js
 module.exports = {
-  PREFIX: process.env.PREFIX || "!",
+  PREFIX: "!",
+  // IMPORTANTE: Adicione o(s) JID(s) completo(s) do(s) seu(s) administrador(es) aqui.
+  // Exemplo: "5551999999999@s.whatsapp.net"
+  ADMIN_JIDS: ["555185156039@s.whatsapp.net"] // <-- COLOQUE SEU NÚMERO ADMIN AQUI
 };
